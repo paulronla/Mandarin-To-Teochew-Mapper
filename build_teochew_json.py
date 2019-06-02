3d0323970c94c0e5a90d6b6a16c9fcf7c60ae38b
paulronla <50727954+paulronla@users.noreply.github.com>
2019-06-01 23:58:49 -0500

    daily commit, working on parsing HTML, commented out JSON portion

--- build_teochew_json.py
+++ build_teochew_json.py
@@ -1,20 +1,55 @@
 import json
+from html.parser import HTMLParser
 
-teochewDict = {
-	"一": {
-		"yi1": "zêg8|ig4|iao1"
-	},
-	"弹": {
-		"dan4": "tang5",
-		"tan2": "tang5|duan7"
-	}
-}
+class TeochewHTMLParser(HTMLParser):
+    
+    def __init__(self):
+        super().__init__()
+        self.last_starttag = None
+        self.last_endtag = None
+        self.teochewDict = {}
+        self.chineseChar = None
+        self.chineseCharEntry = {}
+        self.last_listItemKey = None
+        
 
-with open('teochewDict.json', 'w', encoding='utf-8') as f:
-    json.dump(teochewDict, f, ensure_ascii=False, indent=4)
+    def handle_starttag(self, tag, attrs):
+        self.last_starttag = tag
 
-with open('teochewDict.json', 'r', encoding='utf-8') as f:
-    teochewDict = json.load(f)
+    def handle_endtag(self, tag):
+        self.last_endtag = tag
 
-print(teochewDict['弹']['tan2'])
+    def handle_data(self, data):
+        if self.last_starttag == 'p':
+            self.chineseChar = data
+        
+        if self.last_starttag == 'b':
+            if data in ('潮州音：', '汕头音：', '拼    音：', '字    义：' ):
+                self.last_listItemKey = data
+            
+            
+        
+
+
+parser = TeochewHTMLParser()
+
+
+
+#teochewDict = {
+#	"一": {
+#		"yi1": "zêg8|ig4|iao1"
+#	},
+#	"弹": {
+#		"dan4": "tang5",
+#		"tan2": "tang5|duan7"
+#	}
+#}
+
+#with open('teochewDict.json', 'w', encoding='utf-8') as f:
+#    json.dump(teochewDict, f, ensure_ascii=False, indent=4)
+
+#with open('teochewDict.json', 'r', encoding='utf-8') as f:
+#    teochewDict = json.load(f)
+
+#print(teochewDict['弹']['tan2'])
 
